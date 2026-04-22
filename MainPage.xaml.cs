e39182c6bfdfcbd4ad16764ef1d41ab05e59fd23
connie-sykes <constanza.hidalgo@usm.cl>
2026-04-22 18:48:10 -0400

    Color text changes, Added color names and package

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,10 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	// Iniciar instancia de libreria ColorNameSharp para cargar la lista de nombres de colores
+	ColorNamesSharp.ColorNames colorNames = new ColorNamesSharp.ColorNamesBuilder()
+		.LoadDefault()
+		.Build();
 	// Se llama cada vez que se mueve un slider
 	private void OnSliderCambiado(object sender, ValueChangedEventArgs e)
 	{
@@ -33,7 +37,7 @@ public partial class MainPage : ContentPage
 	private async void OnCopiarHexClicked(object sender, EventArgs e)
 	{
 		await Clipboard.SetTextAsync(LabelHex.Text);
-		await DisplayAlert("Copiado", LabelHex.Text + " copiado al portapapeles", "OK");
+		await DisplayAlertAsync("Copiado", LabelHex.Text + " copiado al portapapeles", "OK");
 	}
 
 	// Aplica el color al fondo y actualiza el label hex
@@ -43,5 +47,27 @@ public partial class MainPage : ContentPage
 
 		LabelHex.Text = hex;
 		FondoColor.Color = Color.FromRgb(r, g, b);
+		// Asigna nombre al color actualizando el label Nombre Color
+		string colorNameFromHex= colorNames.FindClosestColorName(hex);
+		NombreColor.Text = colorNameFromHex;
+
+		// Si el color es muy oscuro, el texto cambia a blanco para ser legible
+		int darkness = 30;
+		if ((r<darkness) && (g<darkness) && (b<darkness))
+		{
+			LabelHex.TextColor = Color.FromRgb(255, 255, 255);
+			NombreColor.TextColor = Color.FromRgb(255, 255, 255);
+			Rojo.TextColor = Color.FromRgb(255, 255, 255);
+			Verde.TextColor = Color.FromRgb(255, 255, 255);
+			Azul.TextColor = Color.FromRgb(255, 255, 255);
+		}
+		else
+		{
+			LabelHex.TextColor = Color.FromRgb(0, 0, 0);
+			NombreColor.TextColor = Color.FromRgb(0, 0, 0);
+			Rojo.TextColor = Color.FromRgb(0, 0, 0);
+			Verde.TextColor = Color.FromRgb(0, 0, 0);
+			Azul.TextColor = Color.FromRgb(0, 0, 0);
+		}
 	}
 }
